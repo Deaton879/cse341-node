2ebdfbee5d7a17de5394cd896c3b0605c8626e80
Dallas <deaton879@byui.edu>
2021-05-15 01:02:10 -0400

    Add/delete products working

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('609f53e7681bb23504d0b94a')
     .then(user => {
       req.user = user;
       next();

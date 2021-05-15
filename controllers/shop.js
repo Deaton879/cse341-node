f6d17e88ae31a88840e68672d4d25dc7103a3ab0
Dallas <deaton879@byui.edu>
2021-05-15 03:52:52 -0400

    Done week 4

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,6 +48,7 @@ exports.getCart = (req, res, next) => {
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
+      console.log(user.cart.items);
       const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
